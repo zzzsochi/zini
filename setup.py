eb7a82e29b97359fbcb34a2b9b0304fbb066ebbb
Alexander Zelenyak <zzz.sochi@gmail.com>
2016-01-23 22:13:41 +0300

    Bump version: 1.0.1 → 1.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='zini',
-    version='1.0.1',
+    version='1.0.2',
     description='INI-files parser with schemes and types',
     # long_description=README,
     classifiers=[
